68a43711484a7fb7210be5aa1f448e36fb2db2f8
anushervonodilov <anushboy2000@gmail.com>
2023-05-31 19:10:42 -0400

    updates

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         for (int i = 0; i < 20; i++) {
             System.out.println("Hello world");
-            Thread.sleep(3000);
+            Thread.sleep(4000);
         }
 
     }
